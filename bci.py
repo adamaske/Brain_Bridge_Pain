8b32eedfd7f269c207fdf21ad9ac9a3a47b04758
Adam Aske <adamaske@live.no>
2023-05-10 04:03:15 +0200

    Now its working really, fixed working dir mistake

--- bci.py
+++
@@ -1,8 +0,0 @@
-
-from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, BrainFlowPresets
-
-def main():
-    BoardShim.enable_dev_board_logger()
-    
-    if __name__ == "__main__":
-        main()
